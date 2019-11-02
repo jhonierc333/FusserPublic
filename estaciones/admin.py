6de72b7a91bcd0a3abdc75bcbc423a4e7eec60e5
Fusser <jhonierc333@gmail.com>
2019-11-02 18:45:27 -0500

    cambio

--- estaciones/admin.py
+++ estaciones/admin.py
@@ -8,15 +8,5 @@ from estaciones.models import Service
 
 
 admin.site.register(Station)
-@admin.register(Product)
-@admin.register(Service)
-
-
-
-class ProductAdmin(admin.ModelAdmin):
-	list_display = ('id_product', 'product_name', 'price', 'detail')
-
-
-
-class ServiceAdmin(admin.ModelAdmin):
-	list_display = ('id_service', 'service_name', 'price', 'detail')
+admin.site.register(Product)
+admin.site.register(Service)
