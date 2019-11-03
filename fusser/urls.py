0111eb7c8926b05d4f3d76d31e8981c83eb77567
Fusser <jhonierc333@gmail.com>
2019-11-03 10:39:19 -0500

    estilos contact

--- fusser/urls.py
+++ fusser/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from fusser.views import contact
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     url(r'^contact/', contact, name='contact'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
