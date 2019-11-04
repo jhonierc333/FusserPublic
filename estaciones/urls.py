c95d487e669537ca5a32be7e899a67821c10f59b
Fusser <jhonierc333@gmail.com>
2019-11-03 23:10:49 -0500

    estilos

--- estaciones/urls.py
+++ estaciones/urls.py
@@ -5,11 +5,11 @@ from estaciones import views
 
 urlpatterns = [
 	url(r'^nueva$', views.crear_estacion, name='crear_estacion'),
-	url(r'^nuevo_producto$', views.crear_producto, name='crear_producto'),
+	url(r'^nuevo_producto$', views.crear_producto.as_view(), name='crear_producto'),
 	url(r'^listar_producto$', views.ProductoList.as_view(), name='listar_productos'),
 	url(r'^eliminar_producto/(?P<pk>[\w]+)$', views.ProductoDelete.as_view(), name='eliminar_producto'),
 
-	url(r'^nuevo_servicio$', views.crear_servicio, name='crear_servicio'),
+	url(r'^nuevo_servicio$', views.crear_servicio.as_view(), name='crear_servicio'),
 	url(r'^listar_servicios$', views.ServiceList.as_view(), name='listar_servicios'),
 	url(r'^eliminar_servicio/(?P<pk>[\w]+)$', views.ServiceDelete.as_view(), name='eliminar_servicio'),
 
--- estaciones/views.py
+++ estaciones/views.py
@@ -24,33 +24,17 @@ def crear_estacion(request):
 
 	return render(request, 'estacion_form.html', {'form':form})
 
-def crear_producto(request):
-	if request.method == 'POST':
-		form = ProductForm(request.POST)
+class crear_producto(generic.CreateView):
+	model = Product
+	form_class = ProductForm
+	template_name = 'productos_form.html'
+	success_url = reverse_lazy('estaciones:listar_productos')
 
-		if form.is_valid():
-			form.save()
-		#return redirect('usuario:usuario_creado')
-
-	else:
-		form = ProductForm()
-
-
-	return render(request, 'productos_form.html', {'form':form})
-
-def crear_servicio(request):
-	if request.method == 'POST':
-		form = ServiceForm(request.POST)
-
-		if form.is_valid():
-			form.save()
-		#return redirect('usuario:usuario_creado')
-
-	else:
-		form = ServiceForm()
-
-
-	return render(request, 'servicios_form.html', {'form':form})
+class crear_servicio(generic.CreateView):
+	model = Service
+	form_class = ServiceForm
+	template_name = 'servicios_form.html'
+	success_url = reverse_lazy('estaciones:listar_servicios')
 
 class ServiceList(generic.ListView):
 	model = Service
